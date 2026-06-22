e21b904cf8f2596ce5ae0da22928a000ba70480a
martin252010 <rapettimartin00@gmail.com>
2026-06-22 11:37:03 -0300

    Add files via upload

--- Parte A TP int.py
+++ Parte A TP int.py
@@ -1,14 +1,22 @@
-def nombre_valido(nombre):
-  return len(nombre) >= 2 and nombre.isalpha() 
+#PARTE A
+def nombre_valido(nombre):   
+    if len(nombre) >= 3 and nombre.isalpha():
+        devolver=True
+    else:
+        devolver=False
+    return devolver
 def crear_codename(nombre, nivel):
   return nombre[0:3].upper() + "-Lv" + str(nivel)
 def vida_maxima(nivel):
   r = 100 + nivel ** 2 * 5
   return r
 
-
+#programa principal
 nombre=str(input("ingrese nombre"))
 nivel=int(input("ingrese nivel"))
 C1=nombre[0:3].upper() 
-print(C1 + " " + nivel)
-print(vida_maxima(nivel))
+if nombre_valido(nombre)==True:
+    print(crear_codename(nombre, nivel))
+    print(vida_maxima(nivel))
+else:
+    print("error")
