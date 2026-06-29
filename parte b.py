d80f057047a1aa5e92f06726a15261945c383ace
magaescudero <ma.escudero@colegiocalasanz.edu.ar>
2026-06-29 11:36:19 -0300

    Add files via upload

--- parte b.py
+++ parte b.py
@@ -1,28 +1,32 @@
 # ===== PARTE B =====
 
-def ClasificarArma():
-    return    # TODO: if/elif/else -> "Legendaria"/"Media"/"Debil"
-    print("1. Legendaria")
-    print("2. Media")
-    print("3. Débil")
-    opción=int (input("Elige una opción"))
-    if opción==1:
+def ClasificarArma(poder):
+    # TODO: if/elif/else -> "Legendaria"/"Media"/"Debil"
+    if poder>1000:
         print("El arma legendaria es mágica y capaz de destruir en pocos golpes al enemigo")
         devolver = "Legendaria"
-    elif opción==2:
+    elif poder<1000 and poder > 500:
         print("El arma media posee una gran capacidad de ganar ventaja contra el enemigo, sin ser del todo mágica")
         devolver= "Media"
-    elif opción==3:
+    elif poder < 500:
         print ("El armma débil no contiene magia pero si sos un guerrero nato, ganarías fácilmente")
         devolver= "Débil"
     return devolver
 
 def EsCritico(es_magica, nivel):
-
-    return    # TODO: es_magica or nivel >= 10
+    if es_magica or nivel>=10:
+        devolver= True
+    else:
+        devolver= False
+    return devolver
     
 
 def dano_base(ataque, poder, defensa):
-    return    # TODO: (ataque + poder) - defensa
+    dano= (ataque+poder)-defensa
+    return  dano
 def dano_total(ataque, poder, defensa, critico):
-    return    # TODO: si critico -> dano_base(...) * 2 ; si no -> dano_base(...)
+    if critico == True:
+        devolver= dano_base(ataque, poder, defensa)
+    elif critico == False:
+        devolver = dano_base(ataque, poder, defensa)
+    return devolver
